89bd5bb7880f296868384002f5e016440172f214
slepin <1294700694@qq.com>
2016-11-30 22:34:23 +0800

    test

---
+++ main/src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/11/30.
+ * just for test.
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("test succ!");
+    }
+}

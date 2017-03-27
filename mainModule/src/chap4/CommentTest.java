a9859207ae1d6a8e49defe0302a543ac447486ee
Jianghao Zhang <1294700694@qq.com>
2017-03-27 08:37:13 +0800

    a little modify

--- mainModule/src/chap4/CommentTest.java
+++ mainModule/src/chap4/CommentTest.java
@@ -4,7 +4,7 @@ package chap4;
  * A <code>CommentTest</code> object represents how to <strong>annotate</strong> package,public class or interface,public or protected
  * constructor as well as method,public or protected field.<br>
  * This class is related to {@link chap4.EmployeeTest#main(String[])}.<br>
- * Just a nonsense toy program,please don't trait it seriously.
+ * Just a nonsense toy program,don't trait it seriously.
  *
  * @author JianghaoZhang
  * @version 1.6
@@ -16,7 +16,7 @@ public class CommentTest {
      * @param b 减数
      * @return 余数
      * @see chap4.Employee#raiseSalary(double)
-     * @see <a href="http://www.cnblogs.com/zhangjianghao">Author's blog home page</a>
+     * @see <a href="http://www.cnblogs.com/zhangjianghao">home page of Author's blog</a>
      * @see "注释测试类"
      * @deprecated Use <code>newMinus()</code> instead.
      */

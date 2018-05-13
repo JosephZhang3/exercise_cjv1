eeb4cbb24e14debdff4ab801a6b3b4545c5b97ec
jianghao zhang <1294700694@qq.com>
2018-05-14 00:33:44 +0800

    good night

--- mainModule/src/chap7/simpleframe/SimpleFrameTest.java
+++ mainModule/src/chap7/simpleframe/SimpleFrameTest.java
@@ -14,19 +14,21 @@ public class SimpleFrameTest {
             public void run() {
                 SimpleFrame sf = new SimpleFrame();
                 Toolkit tool = sf.getToolkit();
-                Image img = tool.getImage("C:\\Users\\zhangjianghao\\Downloads\\funface.jpg");//用滑稽表情做图标，嘿嘿
+                System.out.println(SimpleFrame.class.getResource("/") + "chap7/funface.jpg");//      file:/C:/git-repos/exercise_cjv1/compile/chap7/funface.jpg
+                Image img = tool.getImage("C:/git-repos/exercise_cjv1/compile/chap7/funface.jpg");//注意这里的图像文件的路径必须以操作系统文件系统的盘符开头，不能以  file:/  开头
                 sf.setIconImage(img);
                 sf.setTitle("simpleFrame");
 
-                sf.setLocation(220, 110);//定义组件大小(宽度和高度)
-                sf.setBounds(440, 220, 500, 400);//定义组件位置和大小 x,y,w,h
+                sf.setLocation(220, 110);//定位组件 x,y
+                sf.setBounds(440, 220, 500, 400);//定位组件和定义组件大小 参数依次为x,y,width,height
                 sf.setLocationByPlatform(true);//让窗口系统根据底层平台自动决定窗口的位置
 
-                //会在控制台打印出true  类型为boolean的属性，获取方法由is开头
+                //会在控制台打印出true
+                //注意：类型为boolean的属性，获取方法由is开头，而不是get
                 System.out.println(sf.isLocationByPlatform());
 
-                sf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                sf.setVisible(true);
+                sf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//定义用户关闭框架时的响应动作，这里是让程序简单地退出
+                sf.setVisible(true);//设定是否可见
             }
         });
     }

a5db391b67a4df1b8efcccacf835da0de7232a34
jianghao zhang <1294700694@qq.com>
2018-05-14 02:21:22 +0800

    good night

---
+++ mainModule/src/chap7/message/Message.java
@@ -0,0 +1,51 @@
+package chap7.message;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * 在组件中显示信息
+ */
+public class Message {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new MessageFrame();
+                frame.setTitle("message component");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
+
+class MessageFrame extends JFrame {
+    public MessageFrame() {
+        add(new MessageComponent());
+        pack();//使用默认的首选大小，而不是自定义大小
+    }
+
+}
+
+class MessageComponent extends JComponent {
+    public static final int MESSAGE_X = 275;
+    public static final int MESSAGE_Y = 200;
+
+    private static final int DEFAULT_WIDTH = 600;
+    private static final int DEFAULT_HEIGHT = 500;
+
+    /**
+     * 绘制组件
+     *
+     * @param g 图形类，度量单位是像素
+     */
+    public void paintComponent(Graphics g) {
+        g.drawString("Still a helloworld app", MESSAGE_X, MESSAGE_Y);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}

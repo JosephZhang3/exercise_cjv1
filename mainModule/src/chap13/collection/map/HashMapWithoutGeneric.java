3e87e8de6dfb2d98ea5b763876bbad4a6fe03653
张江浩 <1294700694@qq.com>
2018-11-02 15:36:19 +0800

    review HashMap+1

--- mainModule/src/chap13/collection/map/HashMapWithoutGeneric.java
+++ mainModule/src/chap13/collection/map/HashMapWithoutGeneric.java
@@ -5,26 +5,27 @@ import java.util.*;
 public class HashMapWithoutGeneric {
     public static void main(String[] args) {
         Map<String, Object> hmDemo = new HashMap<>();
+
+
         // 添加数据
         hmDemo.put("a", "hello");
         hmDemo.put("b", 90);
         hmDemo.put("c", true);
         hmDemo.put("d", 9.65f);
         hmDemo.put("e", new Book());
-
         // 删除某项数据
         hmDemo.remove("b");
         // 删除所有数据
 //        hmDemo.clear();
 
 
-        // 按键查找数据
+        // 判断键是否在键值对中存在，然后按键查找数据
         if (hmDemo.containsKey("d")) {
             System.out.println(hmDemo.get("d"));
         }
-        // 按值查找数据
+        // 判断值是否在键值对中存在
         if (hmDemo.containsValue(true)) {
-            System.out.println(hmDemo.get("c"));
+            System.out.println("值true在键值对中存在");
         }
 
 
@@ -46,7 +47,7 @@ public class HashMapWithoutGeneric {
         }
 
 
-        // 遍历键获取值
+        // 遍历键，然后根据键获取值
         Set skeys = hmDemo.keySet();
         Iterator sbowl = skeys.iterator();
         Collection svalues = hmDemo.values();
@@ -56,6 +57,8 @@ public class HashMapWithoutGeneric {
                 System.out.println(sbowl.next() + "->" + sbottle.next());
             }
         }
+
+
         // 集合的长度
         System.out.println("集合的长度为：" + hmDemo.size());
     }

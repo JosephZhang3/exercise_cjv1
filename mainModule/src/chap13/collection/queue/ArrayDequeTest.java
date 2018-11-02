1981c1e25b2b81cc47fd085d3f5b47f8a3482ed8
张江浩 <1294700694@qq.com>
2018-11-03 00:18:28 +0800

    ArrayDeque 数组双向队列

---
+++ mainModule/src/chap13/collection/queue/ArrayDequeTest.java
@@ -0,0 +1,43 @@
+package chap13.collection.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+/**
+ * 双端队列Deque的实现类，以数组方式实现
+ * ArrayDeque实现了Deque接口，而Deque接口又继承自Queue接口
+ */
+public class ArrayDequeTest {
+
+    public static void main(String[] args) {
+        Deque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.addFirst(5);
+        arrayDeque.addFirst(7);
+        arrayDeque.addFirst(8);
+        arrayDeque.add(20);//默认在队列尾部添加新元素
+        arrayDeque.addLast(30);
+
+        for (Integer i : arrayDeque) {
+            System.out.println(i);
+        }
+
+        System.out.println(arrayDeque.getFirst());
+        System.out.println(arrayDeque.getLast());
+
+        /**删除首部第一个元素**/
+        arrayDeque.removeFirst();
+        /**删除尾部第一个元素**/
+        arrayDeque.removeLast();
+
+        System.out.println("执行出列操作前...");
+        for (Iterator it = arrayDeque.iterator(); it.hasNext(); ) {
+            System.out.println(it.next());
+        }
+        arrayDeque.pop();//把首部第一个元素丢出去，等价于removeFirst()方法
+        System.out.println("执行出列操作之后...");
+        for (Iterator it = arrayDeque.iterator(); it.hasNext(); ) {
+            System.out.println(it.next());
+        }
+    }
+}

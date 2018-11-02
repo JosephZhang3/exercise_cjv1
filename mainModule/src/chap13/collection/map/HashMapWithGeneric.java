05373a9beef7bb9352e2c41e1e509052f25e4843
张江浩 <1294700694@qq.com>
2018-11-02 15:31:29 +0800

    review HashMap

--- mainModule/src/chap13/collection/map/HashMapWithGeneric.java
+++ mainModule/src/chap13/collection/map/HashMapWithGeneric.java
@@ -1,30 +1,33 @@
 package chap13.collection.map;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 使用泛型的HashMap
  */
 public class HashMapWithGeneric {
-	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<String, String>();
-		Example first = new Example("001", "赵");
-		Example second = new Example("002", "钱");
-		Example third = new Example("003", "孙");
-		Example fourth = new Example("004", "李");
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        Example first = new Example("001", "赵");
+        Example second = new Example("002", "钱");
+        Example third = new Example("003", "孙");
+        Example fourth = new Example("004", "李");
 
-		map.put(first.getMyid(), first.getMyname());
-		map.put(second.getMyid(), second.getMyname());
-		map.put(third.getMyid(), third.getMyname());
-		map.put(fourth.getMyid(), fourth.getMyname());
+        map.put(first.getMyid(), first.getMyname());
+        map.put(second.getMyid(), second.getMyname());
+        map.put(third.getMyid(), third.getMyname());
+        map.put(fourth.getMyid(), fourth.getMyname());
 
-		Set<String> set = map.keySet();
-		Iterator<String> it = set.iterator();
-		System.out.println("HashMap类实现的Map集合，无序：");
-		while (it.hasNext()) {
-			String id = it.next();
-			String name = map.get(id);
-			System.out.println(id + "\t" + name);
-		}
-	}
+        Set<String> set = map.keySet();
+        Iterator<String> it = set.iterator();
+        System.out.println("HashMap类实现的Map集合，无序：");
+        while (it.hasNext()) {
+            String id = it.next();
+            String name = map.get(id);
+            System.out.println(id + "\t" + name);
+        }
+    }
 }
--- mainModule/src/chap13/collection/map/HashMapWithoutGeneric.java
+++ mainModule/src/chap13/collection/map/HashMapWithoutGeneric.java
@@ -3,57 +3,61 @@ package chap13.collection.map;
 import java.util.*;
 
 public class HashMapWithoutGeneric {
-	public static void main(String[] args) {
-		Map hmDemo = new HashMap();
-		// 添加数据
-		hmDemo.put("a", "hello");
-		hmDemo.put("b", 90);
-		hmDemo.put("c", true);
-		hmDemo.put("d", 9.65f);
-		hmDemo.put("e", new Book());
+    public static void main(String[] args) {
+        Map<String, Object> hmDemo = new HashMap<>();
+        // 添加数据
+        hmDemo.put("a", "hello");
+        hmDemo.put("b", 90);
+        hmDemo.put("c", true);
+        hmDemo.put("d", 9.65f);
+        hmDemo.put("e", new Book());
 
-		// 删除某项数据
-		hmDemo.remove("b");
-		// 删除所有数据
-		hmDemo.clear();
+        // 删除某项数据
+        hmDemo.remove("b");
+        // 删除所有数据
+//        hmDemo.clear();
 
-		// 按键查找数据
-		if (hmDemo.containsKey("d")) {
-			System.out.println(hmDemo.get("d"));
-		}
-		// 按值查找数据
-		if (hmDemo.containsValue(true)) {
-			System.out.println(hmDemo.get("c"));
-		}
 
-		// 集合的数据显示
+        // 按键查找数据
+        if (hmDemo.containsKey("d")) {
+            System.out.println(hmDemo.get("d"));
+        }
+        // 按值查找数据
+        if (hmDemo.containsValue(true)) {
+            System.out.println(hmDemo.get("c"));
+        }
 
-		// 显示所有的key
-		Set allkeys = hmDemo.keySet();
-		Iterator bowl = allkeys.iterator();
-		while (bowl.hasNext()) {
-			System.out.println(bowl.next());
-		}
 
-		// 显示所有的value
-		Collection values = hmDemo.values();
-		Iterator bottle = values.iterator();
-		while (bottle.hasNext()) {
-			System.out.println(bottle.next());
-		}
+        // 显示所有的key
+        Set allkeys = hmDemo.keySet();
+        for (Object allkey : allkeys) {
+            System.out.println(allkey);
+        }
+        // 显示所有的value
+        Collection values = hmDemo.values();
+        for (Object value : values) {
+            System.out.println(value);
+        }
+        //显示所有的key-value对
+        Set<Map.Entry<String, Object>> entrys = hmDemo.entrySet();
+        for (Map.Entry<String, Object> entry : entrys) {
+            System.out.println("键-->" + entry.getKey());
+            System.out.println("值-->" + entry.getValue());
+        }
 
-		// 循环键从而显示值
-		Set skeys = hmDemo.keySet();
-		Iterator sbowl = skeys.iterator();
-		Collection svalues = hmDemo.values();
-		Iterator sbottle = svalues.iterator();
-		for (int i = 0; i < hmDemo.size(); i++) {
-			while (sbowl.hasNext()) {
-				System.out.println(sbowl.next() + "->" + sbottle.next());
-			}
-		}
-		// 统计集合的长度
-		System.out.println("集合的长度为：" + hmDemo.size());
-	}
+
+        // 遍历键获取值
+        Set skeys = hmDemo.keySet();
+        Iterator sbowl = skeys.iterator();
+        Collection svalues = hmDemo.values();
+        Iterator sbottle = svalues.iterator();
+        for (int i = 0; i < hmDemo.size(); i++) {
+            while (sbowl.hasNext()) {
+                System.out.println(sbowl.next() + "->" + sbottle.next());
+            }
+        }
+        // 集合的长度
+        System.out.println("集合的长度为：" + hmDemo.size());
+    }
 
 }

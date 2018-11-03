0b24c6366dc7401367bc53f620c6ab9b5f1c8ff0
张江浩 <1294700694@qq.com>
2018-11-03 12:54:59 +0800

    生产者消费者 design pattern

--- mainModule/src/chap14/customer_producer_pattern/Producer.java
+++ mainModule/src/chap14/customer_producer_pattern/Producer.java
@@ -1,7 +1,6 @@
-package chap14.customer_producer_problem;
+package chap14.customer_producer_pattern;
 
-public class Producer extends Thread
-{
+public class Producer extends Thread {
     // 每次生产的产品数量
     private int num;
 
@@ -9,41 +8,34 @@ public class Producer extends Thread
     private Storage storage;
 
     // 构造函数，设置仓库
-    public Producer(Storage storage)
-    {
+    public Producer(Storage storage) {
         this.storage = storage;
     }
 
     // 线程run函数
-    public void run()
-    {
+    public void run() {
         produce(num);
     }
 
     // 调用仓库Storage的生产函数
-    public void produce(int num)
-    {
+    public void produce(int num) {
         storage.produce(num);
     }
 
     // get/set方法
-    public int getNum()
-    {
+    public int getNum() {
         return num;
     }
 
-    public void setNum(int num)
-    {
+    public void setNum(int num) {
         this.num = num;
     }
 
-    public Storage getStorage()
-    {
+    public Storage getStorage() {
         return storage;
     }
 
-    public void setStorage(Storage storage)
-    {
+    public void setStorage(Storage storage) {
         this.storage = storage;
     }
 }  
---
+++ mainModule/src/chap14/customer_producer_pattern/Storage.java
@@ -0,0 +1,71 @@
+package chap14.customer_producer_pattern;
+
+import java.util.LinkedList;
+
+public class Storage {
+    // 仓库最大存储量
+    private final int MAX_SIZE = 10;
+
+    // 仓库存储的载体
+    private LinkedList<Object> list = new LinkedList<Object>();
+
+    // 生产num个产品(入库)
+    public void produce(int num) {
+        // 同步代码块
+        synchronized (list) {
+            // 如果仓库剩余容量不足
+            while (list.size() + num > MAX_SIZE) {
+                System.out.println("【要生产的产品数量】:" + num + "\t【库存量】:"
+                        + list.size() + "\t暂时不能执行生产任务!");
+                try {
+                    // 由于条件不满足,list资源处于等待状态
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // 仓库剩余容量充足的情况下，生产num个产品
+            for (int i = 1; i <= num; ++i) {
+                list.add(new Object());
+            }
+            System.out.println("【已经生产产品数】:" + num + "\t【现仓储量为】:" + list.size());
+            list.notifyAll();
+        }
+    }
+
+    // 消费num个产品（出库）
+    public void consume(int num) {
+        // 同步代码段
+        synchronized (list) {
+            // 如果仓库存储量不足
+            while (list.size() < num) {
+                System.out.println("【要消费的产品数量】:" + num + "\t【库存量】:"
+                        + list.size() + "\t暂时不能执行消费任务!");
+                try {
+                    // 由于条件不满足，消费阻塞
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // 消费条件满足情况下，消费num个产品
+            for (int i = 1; i <= num; ++i) {
+                list.remove();
+            }
+            System.out.println("【已经消费产品数】:" + num + "\t【现仓储量为】:" + list.size());
+            list.notifyAll();
+        }
+    }
+
+    // get/set方法
+    public LinkedList<Object> getList() {
+        return list;
+    }
+
+    public void setList(LinkedList<Object> list) {
+        this.list = list;
+    }
+
+}

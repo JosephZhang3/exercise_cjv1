ad5b2759fc17ef667be6823eda59cd1b4dc70af4
zhangjh@sijibao.com <314159sunshine>
2020-01-04 15:52:49 +0800

    adjust log level

--- code/src/chap11/log/SimpleLoggerTest.java
+++ code/src/chap11/log/SimpleLoggerTest.java
@@ -5,18 +5,37 @@ import java.util.logging.Logger;
 
 public class SimpleLoggerTest {
 
-    public static final Logger myLogger = Logger.getLogger("chap11.log.SimpleLoggerTest");
+    private static final Logger myLogger = Logger.getLogger("chap11.log.SimpleLoggerTest");
 
     public static void main(String[] args) {
-        myLogger.entering("chap11.log","");
-        Logger.getGlobal().setLevel(Level.ALL);
-        Logger.getGlobal().fine("we called logger once");
-        Logger.getGlobal().info("we called logger once");
+//        Logger.getGlobal().setLevel(Level.INFO);//actually INFO is the jre default level
+//        Logger.getGlobal().info("we called logger once");
+
+        int returnInt = demoMethod("p1", "p2");
+        System.out.println(returnInt);
+
 
     }
 
-    private static int me(){
-
-        return -1;
+    /**
+     * 只有把日志级别设定成 FINER 才能正常调用entering()和exiting()方法
+     * 如何设定？
+     * 修改JDK安装路径下的 jre/lib/logging.properties 文件，
+     * 把 .level= INFO 这一行
+     * 替换成 chap11.log.SimpleLoggerTest.level= FINER
+     * 再把 java.util.logging.ConsoleHandler.level = INFO 这一行
+     * 替换成 java.util.logging.ConsoleHandler.level = FINER
+     * 就一切OK了
+     *
+     * @param param1 第一个参数
+     * @param param2 第二个参数
+     * @return 整型值
+     */
+    private static int demoMethod(String param1, String param2) {
+        myLogger.entering("chap11.log.SimpleLoggerTest", "demoMethod", new Object[]{param1, param2});
+        System.out.println("do sth...");
+        System.out.println("do sth twice...");
+        myLogger.exiting("chap11.log.SimpleLoggerTest", "demoMethod", 5);
+        return 5;
     }
 }
